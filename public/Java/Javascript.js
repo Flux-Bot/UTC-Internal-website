34dcf95dba33eb0c426399c22d45bd065f92e7e3
AlexMayne5 <46424598+AlexMayne5@users.noreply.github.com>
2020-07-14 11:02:17 +0100

    Picture  working and java

--- public/Java/Javascript.js
+++ public/Java/Javascript.js
@@ -1,28 +1,19 @@
-/*
-<script type = "text/javascript">
-    function displayNextImage() {
-        x = (x === images.length - 1) ? 0 : x + 1;
-        document.getElementById("img").src = images[x];
-    }
+ function displayNextImage() {
+              x = (x === images.length - 1) ? 0 : x + 1;
+              document.getElementById("img").src = images[x];
+          }
 
-    function displayPreviousImage() {
-        x = (x <= 0) ? images.length - 1 : x - 1;
-        document.getElementById("img").src = images[x];
-    }
+          function displayPreviousImage() {
+              x = (x <= 0) ? images.length - 1 : x - 1;
+              document.getElementById("img").src = images[x];
+          }
 
-    function startTimer() {
-        setInterval(displayNextImage, 9000);
-    }
+          function startTimer() {
+              setInterval(displayNextImage, 5500);
+          }
 
-    var images = [], x = -1;
-    images[0] = "/picturewheel/1.jfif";
-    images[1] = "/picturewheel/2.jfif";
-    images[2] = "/picturewheel/3.jfif";
-</script>
-
-
-    <body onload = "startTimer()">
-    <img id="img" src="/picturewheel/0.jfif/>
-    <button type="button" onclick="displayPreviousImage()">Previous</button>
-    <button type="button" onclick="displayNextImage()">Next</button>
-//-->
+          var images = [], x = -1;
+          images[0] = "../picture/0.jpg";
+          images[1] = "../picture/1.jpg";
+          images[2] = "../picture/2.jpg";
+          images[3] = "../picture/3.jpg";
