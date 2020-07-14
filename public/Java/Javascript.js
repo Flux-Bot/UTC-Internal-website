e4e0512f12482db2b81afd7a1980eb4bbcf70939
AlexMayne5 <46424598+AlexMayne5@users.noreply.github.com>
2020-07-14 14:28:36 +0100

    aded a clock using java
    NOTE the date is one month behind
    i have not found a fix for this

--- public/Java/Javascript.js
+++ public/Java/Javascript.js
@@ -17,3 +17,26 @@
           images[1] = "../picture/1.jpg";
           images[2] = "../picture/2.jpg";
           images[3] = "../picture/3.jpg";
+
+
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var d = today.getDate();
+    var mo = today.getMonth();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('Time').innerHTML =
+    h + ":" + m
+    document.getElementById('Date').innerHTML =
+    d + "-" + mo
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
