2ebc3756eef66ec568de1fef1b544a810d7e06a7
fluxcabury <mark.williamson02@outlook.com>
2020-09-22 12:51:34 +0100

    Revert "js repair"
    This reverts commit 802859f2698516eb34972c4ad5f446d6ea643111.

--- public/js/main.js
+++ public/js/main.js
@@ -17,15 +17,21 @@ function displayPreviousImage() {
 
 function startTimer() {
 	setInterval(displayNextImage, 5500);
+}
 
+function startTime() {
+	var today = new Date();
 	var h = today.getHours();
 	var m = today.getMinutes();
 	var s = today.getSeconds();
+	var d = today.getDate();
 	var mo = today.getMonth();
 	m = checkTime(m);
 	s = checkTime(s);
 	document.getElementById("Time").innerHTML =
 		h + ":" + m
+	document.getElementById("Date").innerHTML =
+		d + "-" + mo
 	setTimeout(startTime, 1000);
 }
 function checkTime(i) {
